7a551d323cf582e8ef7834694b3d00cf54a7a0c6
Gourab Porey <gourabporey63@gmail.com>
2025-04-24 23:49:39 +0530

    Add logger setup

--- WeatherService.API/Program.cs
+++ WeatherService.API/Program.cs
@@ -1,10 +1,24 @@
+using Azure.Monitor.OpenTelemetry.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// configuration
+builder.Configuration.AddJsonFile("appsettings.json", true, true);
+builder.Configuration.AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", true, true);
+builder.Configuration.AddEnvironmentVariables();
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+var applicationInsightsKey = builder.Configuration["APPLICATIONINSIGHTS_CONNECTION_STRING"];
+if (applicationInsightsKey == null)
+{
+    throw new Exception("ApplicationInsights InstrumentationKey is missing or invalid");
+}
+builder.Services.AddOpenTelemetry().UseAzureMonitor(options => options.ConnectionString = applicationInsightsKey);
 
 var app = builder.Build();
 
